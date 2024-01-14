e48b7f3d9deb6242acdf4ddeb24bfbe3803cab81
Kush Knsl <mrkushkansal@gmail.com>
2024-01-14 14:52:49 +0530

    commit

--- js-hindi-youtube-main/01_basics/07_datesinJs.js
+++ js-hindi-youtube-main/01_basics/07_datesinJs.js
@@ -1,32 +1,23 @@
 // Dates
-
+//Date is an object type in JS
 let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+console.log(myDate.toString());
+// for current date complete details
 
-// let myCreatedDate = new Date(2023, 0, 23)
+//Months starts with 0 in javascript
+let myCreatedDate = new Date(2023, 0, 23)
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
-let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.toLocaleString());
+//locale string is to define the formats of dates requires and changing time zones
 
+
+//time stamps are useful for polls and quizzes
 let myTimeStamp = Date.now()
-
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+//this returns time lapsed(in milliseconds) till now from 1 jan 1970
+console.log(myTimeStamp);
 
 let newDate = new Date()
 console.log(newDate);
 console.log(newDate.getMonth() + 1);
 console.log(newDate.getDay());
-
-// `${newDate.getDay()} and the time `
-
-newDate.toLocaleString('default', {
-    weekday: "long",
-    
-})
-
